b93e1698520f7896e4c8c963f9ef50865d391f2a
ghostonbeat <syabanaminggus7@gmail.com>
2021-10-25 11:10:46 +0700

    buatASD

---
+++ minggu5/cll1.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <stdlib.h>
+using namespace std;
+
+class node {
+public:
+    string nama_mhs;
+    node* next;
+};
+
+void cetak(node* n) {
+    node* F;
+    F = n;
+    int count = 4;
+    while (count > 0) {
+        cout << " " << n->nama_mhs << " ";
+        n = n->next;
+        if (n == F) {
+            count -= 1;
+        }
+    }
+}
+
+int main() {
+    node* first = NULL;
+    node* middle = NULL;
+    node* last = NULL;
+
+    first = new node();
+    middle = new node();
+    last = new node();
+
+    first->nama_mhs = "Yanto";
+    middle->nama_mhs = "Yuli";
+    last->nama_mhs = "Katno";
+
+    first->next = middle;
+    middle->next = last;
+    last->next = first;
+
+    cetak(first);
+}
