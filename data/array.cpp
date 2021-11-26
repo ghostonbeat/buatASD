aa5920724a7dc356175674133832021a0746e6f6
ghostonbeat <syabanaminggus7@gmail.com>
2021-11-26 12:21:12 +0700

    buatASD

--- data/array.cpp
+++ data/array.cpp
@@ -19,7 +19,7 @@ void createEmpty() {
 }
 
 void inputData(info data, int index) {
-    if (index < Maks) {
+    if (index < Maks && index > 0) {
         if (arr(index) == 0) {
             if (data != 0) {
                 arr(index) = data;
