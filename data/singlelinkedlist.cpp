9a0cce8078731bd4c579881ceffebd4ba667610a
ghostonbeat <syabanaminggus7@gmail.com>
2021-11-27 11:14:42 +0700

    buatASD

--- data/singlelinkedlist.cpp
+++ data/singlelinkedlist.cpp
@@ -5,22 +5,67 @@ using namespace std;
 #define Nil NULL
 
 typedef int info;
-typedef struct body *add;
+typedef struct body *addList;
 typedef struct body {
     info data;
-    add next;
+    addList next;
 } list;
 
+typedef struct link *addLink;
 struct link {
-    add head;
+    addList head;
 };
 
-void createHead(link *dat) {
-    dat->head = new list;
+#define H(P) P->head
+#define DAT (*dat)
+#define N(P) P->next
+#define D(P) P->data
+
+void createEmpty(addLink *dat) {
+    (*dat) = new link();
+    H((*dat)) = Nil;
+}
+
+addList addNew(info masuk) {
+    addList NewElmt;
+    NewElmt = new list();
+    D(NewElmt) = masuk;
+    N(NewElmt) = Nil;
+    return NewElmt;
+}
+
+void insertFirst(addLink *dat, info newData) {
+    addList NewElmt = addNew(newData);
+
+    if (H(DAT) == Nil) {
+        H(DAT) = NewElmt;
+    }
+    else {
+        addList Node = H(DAT);
+        N(NewElmt) = Node;
+        H(DAT) = NewElmt;
+    }
+}
+
+void print(addLink role) {
+    addList Temp = H(role);
+    while (Temp != Nil) {
+        cout << D(Temp) << endl;
+        Temp = N(Temp);
+    }
 }
 
 int main() {
-    link tampung;
-    createHead(&tampung);
-    tampung.head->data = 12;
+    addLink tampung;
+    createEmpty(&tampung);
+    addList temp;
+    /*temp = new list();
+    D(temp) = 12;
+    N(temp) = Nil;
+    H(tampung) = temp;*/
+    insertFirst(&tampung, 12);
+    insertFirst(&tampung, 15);
+    insertFirst(&tampung, 20);
+    insertFirst(&tampung, 300);
+    print(tampung);
 }
